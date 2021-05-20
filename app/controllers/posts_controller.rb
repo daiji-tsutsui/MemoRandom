0fd9b70f926b880b505e04a3c9ce254ab3633bb1
Daiji TSUTSUI <daiji.tsutsui417@gmail.com>
2021-05-20 23:57:27 +0900

    Add post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
+  def top
+  end
+
   def new
     @user = current_user
     @post = @user.posts.build()
+  end
+
+  def create
+  end
+
+  def show
+    @user = current_user
+    @post = @user.posts.build()
+  end
+
+  def edit
+    @user = current_user
+    @post = @user.posts.build()
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'posts#top'
   resources :users
 
-  get 'posts/new', to: 'posts#new', as: :post_new
+  get '/top', to: 'posts#top', as: :top
+  get '/posts/new', to: 'posts#new', as: :new_post
+  get '/posts/(:id)', to: 'posts#show', as: :post
+  get '/posts/(:id)/edit', to: 'posts#edit', as: :edit_post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
