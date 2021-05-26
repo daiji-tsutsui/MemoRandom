16b4113d1dc03a51c6e7426e3b9a9e0f734a6229
Daiji TSUTSUI <daiji.tsutsui417@gmail.com>
2021-05-26 22:38:48 +0900

    Add fog config for upload images to AWS:S3

--- app/uploaders/memo_uploader.rb
+++ app/uploaders/memo_uploader.rb
@@ -4,8 +4,11 @@ class MemoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,9 +38,9 @@ class MemoUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png html pdf md txt)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
